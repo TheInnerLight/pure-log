a9d25cc2cfc6c3dd10d4e3ce4a1b1ceb9ef6591e
Phil Curzon <phil@novelfs.org>
2018-10-28 23:14:02 +0000

    Removed the default ToMdc instance

--- src/main/scala/org/novelfs/pure/log/mdc/package.scala
+++ src/main/scala/org/novelfs/pure/log/mdc/package.scala
@@ -5,10 +5,6 @@ import cats.effect.LiftIO
 import cats.mtl.ApplicativeAsk
 
 package object mdc {
-  implicit val identityMdc = new ToMdc[Map[String, String]] {
-    override def toMdc(item: Map[String, String]): Map[String, String] = item
-  }
-
   implicit def mdcLogger[F[_] : LiftIO : Monad, TContext](implicit toMdc : ToMdc[TContext], applicativeAsk : ApplicativeAsk[F, TContext]) : MonadLogger[F] =
     new MdcLogger[F, TContext]()
 }
--- src/test/scala/org/novelfs/pure/log/Test.scala
+++ src/test/scala/org/novelfs/pure/log/Test.scala
@@ -7,6 +7,8 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Test extends FlatSpec with Matchers {
 
+  case class Captain(firstName : String, lastName : String)
+
   "simple logging" should "successfully compile for IO" in {
     import simple._
 
@@ -15,13 +17,14 @@ class Test extends FlatSpec with Matchers {
   }
 
   "mdc logging" should "successfully compile when mdc._ is imported and a simple Map[String, String] context is used" in {
-
     import mdc._
+
+    implicit val stringMapToMdc = new ToMdc[Map[String, String]] {
+      override def toMdc(item: Map[String, String]): Map[String, String] = item
+    }
 
     Logger.log[ReaderT[IO, Map[String, String], ?]](LogLevel.Info)("Hello World!")
       .run( Map("firstName" -> "James", "lastName" -> "Kirk") )
       .unsafeRunSync()
-
   }
-
 }
