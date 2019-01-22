311e1388f2fc20b5a8688c96f6a8a3ce45e1587d
Ben Plommer <ben.plommer@ovoenergy.com>
2019-01-22 10:38:58 +0000

    Add instance methods for log levels to allow more concise use at call site

--- src/main/scala/org/novelfs/pure/log/ApplicativeLogger.scala
+++ src/main/scala/org/novelfs/pure/log/ApplicativeLogger.scala
@@ -7,4 +7,10 @@ import simulacrum.typeclass
 trait ApplicativeLogger[F[_]] {
   def applicative : Applicative[F]
   def log(logLevel: LogLevel)(msg : String) : F[Unit]
+  
+  @inline def trace(msg : String) : F[Unit] = log(LogLevel.Trace)(msg)
+  @inline def debug(msg : String) : F[Unit] = log(LogLevel.Debug)(msg)
+  @inline def info(msg : String) : F[Unit] = log(LogLevel.Info)(msg)
+  @inline def warn(msg : String) : F[Unit] = log(LogLevel.Warn)(msg)
+  @inline def error(msg : String) : F[Unit] = log(LogLevel.Error)(msg)
 }
